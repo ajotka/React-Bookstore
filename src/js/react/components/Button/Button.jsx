e84477b7f4b810a89259473aa591e878ab414ece
AJOTKA <agata.kowalewska@gmail.com>
2018-12-05 13:38:21 +0100

    Publish GHpages

--- src/js/react/components/Button/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './style.scss';
-
-const Button = ({ onClickHandler, text }) => (
-    <button type="button" onClick={onClickHandler}>{text}</button>
-);
-
-export default Button;
--- src/js/react/index.jsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import Router from "./components/Router/Router";
-
-window.addEventListener('load', () => {
-    ReactDOM.render(<Router />, document.getElementById('react-app'));
-});
