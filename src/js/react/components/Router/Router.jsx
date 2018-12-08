b978c128eacaa56729204fcd0d500150bd64bb16
AJOTKA <agata.kowalewska@gmail.com>
2018-12-08 18:12:41 +0100

    GHPages

--- src/js/react/components/Router/Router.jsx
+++ src/js/react/components/Router/Router.jsx
@@ -5,7 +5,7 @@ import AdminPanel from '../AdminPanel/AdminPanel';
 import OrderSummary from "../Order/OrderSummary";
 import PageNotFound from './PageNotFound';
 import Footer from "../Footer/Footer";
-import Package from "../../../../../package.json";
+import PackageJson from "../../../../../package.json";
 
 export default class Router extends React.Component {
 
@@ -14,11 +14,11 @@ export default class Router extends React.Component {
         return (
             <React.Fragment>
 
-                <BrowserRouter basename={Package.basename}>
+                <BrowserRouter>
                     <Switch>
-                        <Route path={`${Package.homepage}/`} component={App} />
-                        <Route path={`${Package.homepage}/admin`} component={AdminPanel} />
-                        <Route path={`${Package.homepage}/order`} component={OrderSummary} />
+                        <Route path={PackageJson.homepage + '/'} component={App} />
+                        <Route path={PackageJson.homepage + '/admin'} component={AdminPanel} />
+                        <Route path={PackageJson.homepage + '/order'} component={OrderSummary} />
                         <Route component={PageNotFound} />
                     </Switch>
                 </BrowserRouter>
